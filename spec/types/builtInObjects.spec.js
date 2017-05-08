53b51dd2196ca6d1d89d4e45fc65d294a875a722
Graeme Sutherland <grasuth@nodestone.io>
2017-05-08 20:50:15 +0000

    null and undefined tests

--- spec/types/builtInObjects.spec.js
+++ spec/types/builtInObjects.spec.js
@@ -28,4 +28,41 @@ describe('Built-in objects', function() {
             expect(NaN).not.toEqual(NaN);
         });
     });
+    
+    describe('undefined', function() {
+       var a;
+       
+       it('is the value of a var not assigned to', function() {
+            expect(a).toBe(undefined);
+       });
+       
+       it('typeof unassigned var', function() {
+            expect(typeof a).toBe('undefined');
+            expect(typeof undefined).toBe('undefined');
+       });
+       
+       it('equals void 0', function () {
+            expect(void 0).toBe(undefined); 
+       });
+    });
+    
+    describe('null', function() {
+        it('has a type of object', function () {
+            expect(typeof null).toBe('object');
+        });
+        it('is equal to undefined', function() {
+            expect(null).toEqual(undefined);
+        });
+        it('is not *exactly* equal to undefined', function() {
+            expect(null).not.toBe(undefined);
+        });
+        it('is not true', function() {
+            expect(!null).toBe(true);
+        });
+        it('null exactly equals null', function() {
+            expect(null).toBe(null);
+        });
+        
+        
+    });
 });
